fc5e2d750d14004775084692d30428bacfc53efd
kmtruong <61531549+kmtruong@users.noreply.github.com>
2021-03-26 15:40:36 -0700

    Minimap - 1st commit
    Have a render texture minimap canvas tied to main camera.

---
+++ Assets/MinimapController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapController : MonoBehaviour
+{
+    public GameObject Canvas;
+    // Update is called once per frame
+    bool minimapOn = false;
+    void Update()
+    {
+
+        if (Input.GetKey("m") && (!minimapOn))
+        {
+            minimapOn = true;
+            Canvas.SetActive(true);
+        }
+            
+
+        if (Input.GetKey("m") && (minimapOn))
+        {
+            minimapOn = false;
+            Canvas.SetActive(false);
+        }
+            
+        
+    }
+}
