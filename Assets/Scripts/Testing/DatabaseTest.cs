cdde3e4fccab26ed4aa3e525d75150a9f02898b8
Jocelyn Zaman <44142922+jocelynzaman@users.noreply.github.com>
2021-03-26 21:47:20 -0700

    Created test scripts
    Created CheckoutTest script to test that items were being added and removed correctly on Oculus
    Created DatabaseTest script to test that item prefabs can be customized and instantiated into the scene
    Created InspectTest to test the ItemInspection text can be updated correctly on Oculus

---
+++ Assets/Scripts/Testing/DatabaseTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatabaseTest : MonoBehaviour
+{
+    public GameObject itemPrefab;
+    private List<string> names;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        names = new List<string>();
+        names.Add("Hello");
+        names.Add("World!");
+        Test();   
+    }
+
+    //Test that Database can set up ItemPrefab
+    private void Test()
+    {
+        GameObject copyPrefab = Instantiate(itemPrefab, transform);
+        copyPrefab.GetComponent<ObjectController>().CreateItemObject(names[0], "abc123", "this is just a test.", 500.00f, 1);
+        //copyPrefab.GetComponent<InspectController>().enabled = true;
+
+        copyPrefab = Instantiate(itemPrefab, transform);
+        copyPrefab.GetComponent<ObjectController>().CreateItemObject(names[1], "def456", "Has a sprinkle of innovation", 1.00f, 1);
+        //copyPrefab.GetComponent<InspectController>().enabled = true;
+    }
+}
