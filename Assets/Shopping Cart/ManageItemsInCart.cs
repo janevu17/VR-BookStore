eb9f5af124343bf60bb7c36b5ec7ca469bdb4bdd
Jocelyn Zaman <44142922+jocelynzaman@users.noreply.github.com>
2021-03-27 22:24:16 -0700

    Updated ManageItemsInCart
    Modified CountObj to be a Coroutine and checked which items were added and removed to update CheckoutUI

--- Assets/Shopping Cart/ManageItemsInCart.cs
+++ Assets/Shopping Cart/ManageItemsInCart.cs
@@ -1,35 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Linq;
 
 public class ManageItemsInCart : MonoBehaviour
 {
-/*    Collider[] inCart;*/
     public GameObject cart;
-    public Collider[] inCart; 
-    void CountObj(Vector3 center, float radius)
+    public CheckoutTest checkoutScript;
+
+    private Collider[] inCart; //items currently in cart
+    private Collider[] checkCart; //recent check of items in cart
+    private IEnumerable<Collider> newItems; //new items added into cart
+    private IEnumerable<Collider> removedItems; //items removed from cart
+    
+    private Vector3 sphereCenter;
+    private float sphereRadius;
+
+    private IEnumerator CheckCart(Vector3 center, float radius)
     {
-        inCart = Physics.OverlapSphere(center, radius);
-        foreach (var hitCollider in inCart)
+        checkCart = Physics.OverlapSphere(center, radius);
+
+        //If newItems array isn't equal to inCart array, then add new items
+        newItems = checkCart.Except(inCart);
+
+        //If removedItems array isn't equal to inCart array, then remove removed items
+        removedItems = inCart.Except(checkCart);
+
+        foreach (Collider items in newItems)
         {
-            if (hitCollider.tag == "TestObj")
+            if (items.tag == "TestObj")
             {
-                Debug.Log(hitCollider.name);
+                checkoutScript.AddItem(items.gameObject);
             }
         }
 
-    }
+        foreach (Collider items in removedItems)
+        {
+            if (items.tag == "TestObj")
+            {
+                checkoutScript.DeleteItem(items.gameObject);
+            }
+        }
 
+        inCart = checkCart;
+
+        yield return new WaitForSeconds(0.1f);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        sphereCenter = cart.transform.position;
+        sphereRadius = 1f;
+        inCart = Physics.OverlapSphere(sphereCenter, sphereRadius);
     }
 
     // Update is called once per frame
     void Update()
     {
-        CountObj(cart.transform.position, 1f);
+        StartCoroutine(CheckCart(sphereCenter, sphereRadius));
     }
 }
