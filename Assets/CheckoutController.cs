8882baeffaecf5144bdf542d4de3849529872ac2
Jocelyn Zaman <44142922+jocelynzaman@users.noreply.github.com>
2021-04-08 00:37:35 -0700

    Created CheckoutController script
    Opens and closes the CheckoutUI

---
+++ Assets/CheckoutController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class CheckoutController : MonoBehaviour
+{
+    private Canvas checkoutCanvas;
+
+    //Controller Input Detection
+    private InputDevice device;
+    private bool buttonPress;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkoutCanvas = transform.GetComponent<Canvas>();
+        checkoutCanvas.enabled = false;
+
+        device = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+        buttonPress = false;
+    }
+
+    //If Exit button is clicked on CheckoutUI, then close CheckoutUI
+    public void CloseCheckout()
+    {
+        checkoutCanvas.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If B button on Oculus is pressed, then open CheckoutUI
+        if (device.TryGetFeatureValue(CommonUsages.menuButton, out buttonPress) && buttonPress)
+        {
+            checkoutCanvas.enabled = true;
+        }
+    }
+}
