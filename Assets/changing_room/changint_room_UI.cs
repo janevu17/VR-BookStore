525908423c4af0acd2562010d26724262faa2de6
janevu17 <39614938+janevu17@users.noreply.github.com>
2021-11-20 17:25:04 -0600

    changingRoomWithModel

--- Assets/changing_room/changint_room_UI.cs
+++ Assets/changing_room/changint_room_UI.cs
@@ -10,8 +10,8 @@ public class changint_room_UI : MonoBehaviour
     [SerializeField]
     private GameObject clothing_row;
     [SerializeField]
-    private List<changing_test> my_clothing_list;
-    [SerializeField]
+    //private List<changing_test> my_clothing_list;
+   // [SerializeField]
     private MeshRenderer clothes;
     private List<GameObject> clothing_list;
     private GameObject store_row;
@@ -27,11 +27,11 @@ public class changint_room_UI : MonoBehaviour
         clothing_list = new List<GameObject>();
         //clothing_list.Add(GameObject.Find("/henry_outfit/long-sleeve").GetComponent<MeshRenderer>());
         //clothing_list.Add(GameObject.Find("/henry_outfit/tshirt"));
-        clothes = GameObject.Find("/henry_outfit/long-sleeve").GetComponent<MeshRenderer>();
-        clothes.enabled = true;
-
-        clothes = GameObject.Find("/henry_outfit/tshirt").GetComponent<MeshRenderer>();
-        clothes.enabled = false;
+      //  clothes = GameObject.Find("/henry_outfit/long-sleeve").GetComponent<MeshRenderer>();
+      //  clothes.enabled = true;
+        
+      //  clothes = GameObject.Find("/henry_outfit/tshirt").GetComponent<MeshRenderer>();
+      //  clothes.enabled = false;
     }
 
     // Update is called once per frame
@@ -39,21 +39,21 @@ public class changint_room_UI : MonoBehaviour
     {
 
     }
-
+    
     public void add_FAQ_to_list(int i)
     {
         //for(int i = 0; i < my_clothing_list.Count; i++)
         //{
-        if (my_clothing_list[i].clothing == null)
-        {
-            print("question or answer null");
-        }
-        else
-        {
-            //store_row = Instantiate(clothing_row, transform);
+      //  if (my_clothing_list[i].clothing == null)
+      //  {
+      //      print("question or answer null");
+      //  }
+      //  else
+      //  {
+      //      //store_row = Instantiate(clothing_row, transform);
             //store_row.transform.Find("Question").GetComponent<Text>().text = my_clothing_list[i].question;
-            clothing_list.Add(store_row);
-        }
+      //      clothing_list.Add(store_row);
+      //  }
         //}
     }
 
---
+++ Assets/changing_room/displayAsset.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class displayAsset : MonoBehaviour
+{
+    public Image displayImage;
+    public Text displayName;
+    public Button button;
+    public void setImage()
+    {
+        displayImage.sprite = button.GetComponent<Button>().image.sprite;
+        displayName.text = button.GetComponent<Button>().image.sprite.name;
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
